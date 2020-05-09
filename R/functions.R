d95b2e98059d764044f3de4bc7a3fccf44346fb0
Junyang Qian <qianjunyang@gmail.com>
2020-05-09 15:31:45 +0800

    Add a predict function

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,142 @@
+#' @export
+predict_snpnet <- function(saved_path, new_genotype_file, new_phenotype_file, phenotype,
+                           gcount_path = NULL, meta_dir = NULL, meta_suffix = ".rda",
+                           covariate_names = NULL, split_col = NULL, split_name = NULL, idx = NULL,
+                           family = NULL,
+                           snpnet_prefix = "output_iter_", snpnet_suffix = ".RData", snpnet_subdir = "results",
+                           configs = list(zstdcat.path = "zstdcat", zcat.path='zcat')) {
+
+  phe_dir <- file.path(saved_path, snpnet_subdir)
+  files_in_dir <- list.files(phe_dir)
+  result_files <- files_in_dir[startsWith(files_in_dir, snpnet_prefix) & endsWith(files_in_dir, snpnet_suffix)]
+  max_iter <- max(as.numeric(gsub(snpnet_suffix, "", gsub(pattern = snpnet_prefix, "", result_files))))
+
+  e <- new.env()
+  load(file.path(saved_path, snpnet_subdir, paste0(snpnet_prefix, max_iter, snpnet_suffix)), envir = e)
+  a0 <- e$a0
+  if (is.null(idx)) idx <- seq_along(a0)
+  beta <- e$beta
+  feature_names <- unique(unlist(sapply(beta, function(x) names(x[x != 0]))))
+  feature_names <- setdiff(feature_names, covariate_names)
+
+  stats <- list()
+  if (!is.null(gcount_path)) {
+    gcount_df <-
+      data.table::fread(gcount_path) %>%
+      dplyr::rename(original_ID = ID) %>%
+      dplyr::mutate(
+        ID = paste0(original_ID, '_', ALT),
+        stats_pNAs  = MISSING_CT / (MISSING_CT + OBS_CT),
+        stats_means = (HAP_ALT_CTS + HET_REF_ALT_CTS + 2 * TWO_ALT_GENO_CTS ) / OBS_CT,
+        stats_msts  = (HAP_ALT_CTS + HET_REF_ALT_CTS + 4 * TWO_ALT_GENO_CTS ) / OBS_CT,
+        stats_SDs   = stats_msts - stats_means * stats_means
+      )
+    stats[["pnas"]]  <- gcount_df %>% dplyr::pull(stats_pNAs)
+    stats[["means"]] <- gcount_df %>% dplyr::pull(stats_means)
+    stats[["sds"]]   <- gcount_df %>% dplyr::pull(stats_SDs)
+    for(key in names(stats)){
+      names(stats[[key]]) <- gcount_df %>% dplyr::pull(ID)
+    }
+  } else {
+    stats[["pnas"]] <- readRDS(file.path(meta_dir, paste0("pnas", meta_suffix)))
+    stats[["means"]] <- readRDS(file.path(meta_dir, paste0("means", meta_suffix)))
+    if (file.exists(file.path(meta_dir, paste0("sds", meta_suffix)))) {
+      stats[["sds"]] <- readRDS(file.path(meta_dir, paste0("sds", meta_suffix)))
+    }
+  }
+
+  ids <- list()
+  ids[["psam"]] <- readIDsFromPsam(paste0(new_genotype_file, '.psam'))
+
+  phe_master <- readPheMaster(new_phenotype_file, ids[['psam']], family, covariate_names, phenotype, NULL, split_col, configs)
+  if (length(covariate_names) > 0) {
+    cov_master <- as.matrix(phe_master[, covariate_names, with = F])
+    cov_no_missing <- apply(cov_master, 1, function(x) all(!is.na(x)))
+    phe_master <- phe_master[cov_no_missing, ]
+  }
+
+  if (is.null(family)) family <- inferFamily(phe_master, phenotype, NULL)
+  if (is.null(configs[["metric"]])) configs[["metric"]] <- setDefaultMetric(family)
+
+  if (is.null(split_col)) {
+    split_name <- "train"
+    ids[["train"]] <- phe_master$ID
+  } else {
+    for (split in split_name) {
+      ids[[split]] <- phe_master$ID[phe_master[[split_col]] == split]
+      if (length(ids[[split]]) == 0) {
+        warning(paste("Split", split, "doesn't exist in the phenotype file. Excluded from prediction.\n"))
+        split_name <- setdiff(split_name, split)
+      }
+    }
+  }
+
+  phe <- list()
+  for (split in split_name) {
+    ids_loc <- match(ids[[split]], phe_master[["ID"]])
+    phe[[split]] <- phe_master[ids_loc]
+  }
+
+  covariates <- list()
+
+  for (split in split_name) {
+    if (length(covariate_names) > 0) {
+      covariates[[split]] <- phe[[split]][, covariate_names, with = FALSE]
+    } else {
+      covariates[[split]] <- NULL
+    }
+  }
+
+  vars <- dplyr::mutate(dplyr::rename(data.table::fread(cmd=paste0(configs[["zstdcat.path"]], ' ', paste0(new_genotype_file, '.pvar.zst'))), 'CHROM'='#CHROM'), VAR_ID=paste(ID, ALT, sep='_'))$VAR_ID
+  pvar <- pgenlibr::NewPvar(paste0(new_genotype_file, '.pvar.zst'))
+  chr <- list()
+  for (split in split_name) {
+    chr[[split]] <- pgenlibr::NewPgen(paste0(new_genotype_file, '.pgen'), pvar = pvar, sample_subset = match(ids[[split]], ids[["psam"]]))
+  }
+  pgenlibr::ClosePvar(pvar)
+
+  features <- list()
+  for (split in split_name) {
+    if (!is.null(covariates[[split]])) {
+      features[[split]] <- data.table::data.table(covariates[[split]])
+      features[[split]][, (feature_names) := prepareFeatures(chr[[split]], vars, feature_names, stats)]
+    } else {
+      features[[split]] <- prepareFeatures(chr[[split]], vars, feature_names, stats)
+    }
+  }
+
+  pred <- list()
+  metric <- list()
+  for (split in split_name) {
+    pred[[split]] <- array(dim = c(nrow(features[[split]]), length(idx)),
+                           dimnames = list(ids[[split]], paste0("s", idx-1)))
+    metric[[split]] <- rep(NA, length(idx))
+    names(metric[[split]]) <- paste0("s", idx-1)
+  }
+
+  response <- list()
+  for (split in split_name) {
+    response[[split]] <- phe[[split]][[phenotype]]
+  }
+
+  for (split in split_name) {
+    for (i in idx) {
+      active_names <- names(beta[[i]])[beta[[i]] != 0]
+      if (length(active_names) > 0) {
+        features_single <- as.matrix(features[[split]][, active_names, with = F])
+      } else {
+        features_single <- matrix(0, nrow(features[[split]]), 0)
+      }
+      pred_single <- a0[[i]] + features_single %*% beta[[i]][active_names]
+      pred[[split]][, i] <- as.matrix(pred_single)
+    }
+    metric[[split]] <- computeMetric(pred[[split]], response[[split]], configs[["metric"]])
+  }
+
+  list(prediction = pred, response = response, metric = metric)
+}
+
+
 #' @importFrom data.table set as.data.table
 #' @importFrom magrittr %>%
 #' @importFrom dplyr n
@@ -25,7 +164,7 @@ inferFamily <- function(phe, phenotype, status){
         family <- "cox"
     } else {
         family <- "gaussian"
-    } 
+    }
     family
 }
 
@@ -80,7 +219,7 @@ readPheMaster <- function(phenotype.file, psam.ids, family, covariates, phenotyp
     rownames(phe.master) <- phe.master$ID
 
     # focus on individuals with non-missing values.
-    phe.no.missing.IDs <- phe.master$ID[ 
+    phe.no.missing.IDs <- phe.master$ID[
         (phe.master[[phenotype]] != -9) & # missing phenotypes are encoded with -9
         (!is.na(phe.master[[phenotype]])) &
         (phe.master$ID %in% psam.ids) # check if we have genotype
@@ -89,7 +228,7 @@ readPheMaster <- function(phenotype.file, psam.ids, family, covariates, phenotyp
         # focus on individuals in training and validation set
         phe.no.missing.IDs <- intersect(
             phe.no.missing.IDs,
-            phe.master$ID[ (phe.master[[split.col]] %in% c('train', 'val')) ]
+            phe.master$ID[ (phe.master[[split.col]] %in% c('train', 'val', 'test')) ]
         )
     }
     if(!is.null(configs[['keep']])){
@@ -97,17 +236,17 @@ readPheMaster <- function(phenotype.file, psam.ids, family, covariates, phenotyp
         phe.no.missing.IDs <- intersect(phe.no.missing.IDs, readPlinkKeepFile(configs[['keep']]))
     }
     checkMissingPhenoWarning(phe.master, phe.no.missing.IDs)
-    
+
     phe.master[ phe.master$ID %in% phe.no.missing.IDs, ]
 }
 
 checkMissingPhenoWarning <- function(phe.master, phe.no.missing.IDs){
-  # Show warning message if there are individuals (in phe file) 
+  # Show warning message if there are individuals (in phe file)
   # that have (genotype or phenotype) missing values.
     phe.missing.IDs <- phe.master$ID[ ! phe.master$ID %in% phe.no.missing.IDs ]
     if(length(phe.missing.IDs) > 0){
         warning(sprintf(
-          'We detected missing values for %d individuals (%s ...).\n', 
+          'We detected missing values for %d individuals (%s ...).\n',
           length(phe.missing.IDs),
           paste(utils::head(phe.missing.IDs, 5), collapse=", ")
         ))
@@ -121,10 +260,10 @@ computeStats <- function(pfile, ids, configs) {
   dir.create(dirname(configs[['gcount.full.prefix']]), showWarnings = FALSE, recursive = TRUE)
   if (file.exists(gcount_tsv_f)) {
       gcount_df <- data.table::fread(gcount_tsv_f)
-  } else {      
+  } else {
       # To run plink2 --geno-counts, we write the list of IDs to a file
       data.frame(ID = ids) %>%
-      tidyr::separate(ID, into=c('FID', 'IID'), sep='_') %>% 
+      tidyr::separate(ID, into=c('FID', 'IID'), sep='_') %>%
       data.table::fwrite(keep_f, sep='\t', col.names=F)
 
       # Run plink2 --geno-counts
@@ -152,7 +291,7 @@ computeStats <- function(pfile, ids, configs) {
           stats_SDs   = stats_msts - stats_means * stats_means
         )
   }
-    
+
   out <- list()
   out[["pnas"]]  <- gcount_df %>% dplyr::pull(stats_pNAs)
   out[["means"]] <- gcount_df %>% dplyr::pull(stats_means)
@@ -160,10 +299,10 @@ computeStats <- function(pfile, ids, configs) {
 
   for(key in names(out)){
       names(out[[key]]) <- gcount_df %>% dplyr::pull(ID)
-  }    
+  }
   out[["excludeSNP"]] <- names(out[["means"]])[(out[["pnas"]] > configs[["missing.rate"]]) | (out[["means"]] < 2 * configs[["MAF.thresh"]])]
   out[["excludeSNP"]] <- out[["excludeSNP"]][ ! is.na(out[["excludeSNP"]]) ]
-    
+
   if (configs[['save']]){
       gcount_df %>% data.table::fwrite(gcount_tsv_f, sep='\t')
       saveRDS(out[["excludeSNP"]], file = file.path(dirname(configs[['gcount.full.prefix']]), "excludeSNP.rda"))
@@ -185,7 +324,7 @@ readBinMat <- function(fhead, configs){
     colnames(M) <- cols
     rownames(M) <- rows
     if (! configs[['save.computeProduct']]) system(paste(
-        'rm', paste0(fhead, '.cols'), paste0(fhead, '.vars.zst'), 
+        'rm', paste0(fhead, '.cols'), paste0(fhead, '.vars.zst'),
         paste0(fhead, '.bin'), sep=' '
     ), intern=F, wait=T)
     M
@@ -198,17 +337,17 @@ computeProduct <- function(residual, pfile, vars, stats, configs, iter) {
   gc_res <- gc()
   if(configs[['KKT.verbose']]) print(gc_res)
 
-  snpnetLogger('Start plink2 --variant-score', indent=3, log.time=time.computeProduct.start)    
+  snpnetLogger('Start plink2 --variant-score', indent=3, log.time=time.computeProduct.start)
   dir.create(file.path(configs[['results.dir']], configs[["save.dir"]]), showWarnings = FALSE, recursive = T)
-    
+
   residual_f <- file.path(configs[['results.dir']], configs[["save.dir"]], paste0("residuals_iter_", iter, ".tsv"))
-    
+
   # write residuals to a file
   residual_df <- data.frame(residual)
   colnames(residual_df) <- paste0('lambda_idx_', colnames(residual))
-  residual_df %>%    
+  residual_df %>%
     tibble::rownames_to_column("ID") %>%
-    tidyr::separate(ID, into=c('#FID', 'IID'), sep='_') %>% 
+    tidyr::separate(ID, into=c('#FID', 'IID'), sep='_') %>%
     data.table::fwrite(residual_f, sep='\t', col.names=T)
 
   # Run plink2 --geno-counts
@@ -231,11 +370,11 @@ computeProduct <- function(residual, pfile, vars, stats, configs, iter) {
   if (! configs[['save.computeProduct']] ) system(paste(
       'rm', residual_f, stringr::str_replace_all(residual_f, '.tsv$', '.log'), sep=' '
   ), intern=F, wait=T)
-    
+
   snpnetLoggerTimeDiff('End plink2 --variant-score.', time.computeProduct.start, indent=4)
 
   rownames(prod.full) <- vars
-  if (configs[["standardize.variant"]]) {    
+  if (configs[["standardize.variant"]]) {
       for(residual.col in 1:ncol(residual)){
         prod.full[, residual.col] <- apply(prod.full[, residual.col], 2, "/", stats[["sds"]])
       }
@@ -250,11 +389,11 @@ KKT.check <- function(residual, pfile, vars, n.train, current.lams, prev.lambda.
   if (is.null(alpha)) alpha <- 1
   if (configs[['KKT.verbose']]) snpnetLogger('Start KKT.check()', indent=1, log.time=time.KKT.check.start)
   prod.full <- computeProduct(residual, pfile, vars, stats, configs, iter) / n.train
-  
+
   if(!is.null(p.factor)){
     prod.full <- sweep(prod.full, 1, p.factor, FUN="/")
   }
-    
+
   if (configs[['KKT.verbose']]) snpnetLoggerTimeDiff('- computeProduct.', indent=2, start.time=time.KKT.check.start)
   num.lams <- length(current.lams)
   if (length(configs[["covariates"]]) > 0) {
@@ -262,7 +401,7 @@ KKT.check <- function(residual, pfile, vars, n.train, current.lams, prev.lambda.
   } else {
     strong.vars <- match(rownames(glmfit$beta), rownames(prod.full))
   }
-  if (configs[['KKT.verbose']]) snpnetLoggerTimeDiff('- strong.vars.', indent=2, start.time=time.KKT.check.start)    
+  if (configs[['KKT.verbose']]) snpnetLoggerTimeDiff('- strong.vars.', indent=2, start.time=time.KKT.check.start)
   weak.vars <- setdiff(1:nrow(prod.full), strong.vars)
 
   if (length(configs[["covariates"]]) > 0) {
@@ -283,7 +422,7 @@ KKT.check <- function(residual, pfile, vars, n.train, current.lams, prev.lambda.
   } else {
     mat.cmp <- matrix(current.lams * max(alpha, 1e-3), nrow = length(weak.vars), ncol = length(current.lams), byrow = T)  # make feasible for ridge
   }
-  if (configs[['KKT.verbose']]) snpnetLoggerTimeDiff('- mat.cmp.', indent=2, start.time=time.KKT.check.start)    
+  if (configs[['KKT.verbose']]) snpnetLoggerTimeDiff('- mat.cmp.', indent=2, start.time=time.KKT.check.start)
 
   # check KKT violation using mat.cmp
   num.violates  <- apply(abs(prod.full[weak.vars, , drop = FALSE]) - mat.cmp, 2, function(x) sum(x > 0, na.rm = T))
@@ -295,7 +434,7 @@ KKT.check <- function(residual, pfile, vars, n.train, current.lams, prev.lambda.
   } else {
     score <- NULL
   }
-  if (configs[['KKT.verbose']]) snpnetLoggerTimeDiff('- score.', indent=2, start.time=time.KKT.check.start) 
+  if (configs[['KKT.verbose']]) snpnetLoggerTimeDiff('- score.', indent=2, start.time=time.KKT.check.start)
 
   out <- list(max.valid.idx = max.valid.idx, score = score)
 
@@ -358,7 +497,7 @@ computeMetric <- function(pred, response, metric.type) {
         metric <- apply(pred, 2, function(p) {
             d <- glmnet::coxnet.deviance(p, response)
             1 - d/d0
-        })  
+        })
     } else if (metric.type == 'C'){
       metric <- apply(pred, 2, function(p) {
         cindex::CIndex(p, response[,1], response[,2])
@@ -374,18 +513,18 @@ checkEarlyStopping <- function(metric.val, max.valid.idx, iter, configs){
     snpnetLogger(sprintf('stopping lag=%g, max.val.1=%g max.val.2=%g', max.valid.idx.lag, max.val.1, max.val.2))
     if (
         (configs[['early.stopping']]) &&
-        (max.valid.idx > configs[['stopping.lag']]) && 
+        (max.valid.idx > configs[['stopping.lag']]) &&
         (max.val.1 > max.val.2)
     ) {
         snpnetLogger(sprintf(
-            "Early stopped at iteration %d (Lambda idx=%d ) with validation metric: %.14f.", 
+            "Early stopped at iteration %d (Lambda idx=%d ) with validation metric: %.14f.",
             iter, which.max(metric.val), max(metric.val, na.rm = T)
         ))
         snpnetLogger(paste0(
-            "Previous ones: ", 
-            paste(metric.val[(max.valid.idx-configs[['stopping.lag']]+1):max.valid.idx], collapse = ", "), 
+            "Previous ones: ",
+            paste(metric.val[(max.valid.idx-configs[['stopping.lag']]+1):max.valid.idx], collapse = ", "),
             "."
-        ), indent=1)        
+        ), indent=1)
         earlyStop <- TRUE
     } else {
         earlyStop <- FALSE
@@ -432,15 +571,15 @@ checkGlmnetPlus <- function(use.glmnetPlus, family) {
 setupConfigs <- function(configs, genotype.pfile, phenotype.file, phenotype, covariates, alpha, nlambda, split.col, p.factor, status.col, mem){
     out.args <- as.list(environment())
     defaults <- list(
-        missing.rate = 0.1, 
-        MAF.thresh = 0.001, 
+        missing.rate = 0.1,
+        MAF.thresh = 0.001,
         nCores = 1,
         glmnet.thresh = 1e-07,
         nlams.init = 10,
         nlams.delta = 5,
-        num.snps.batch = 1000, 
+        num.snps.batch = 1000,
         vzs=TRUE, # geno.pfile vzs
-        increase.size = NULL,        
+        increase.size = NULL,
         standardize.variant = FALSE,
         early.stopping = TRUE,
         stopping.lag = 2,
@@ -451,7 +590,7 @@ setupConfigs <- function(configs, genotype.pfile, phenotype.file, phenotype, cov
         use.glmnetPlus = NULL,
         save = FALSE,
         save.computeProduct = FALSE,
-        prevIter = 0, 
+        prevIter = 0,
         results.dir = NULL,
         meta.dir = 'meta',
         save.dir = 'results',
@@ -477,11 +616,11 @@ setupConfigs <- function(configs, genotype.pfile, phenotype.file, phenotype, cov
     for (name in names(configs)) {
         out[[name]] <- configs[[name]]
     }
-    
+
     # update settings
     out[["early.stopping"]] <- ifelse(out[["early.stopping"]], out[['stopping.lag']], -1)
     if(is.null(out[['increase.size']]))  out[['increase.size']] <- out[['num.snps.batch']]/2
-    
+
     # configure the temp file locations
     #   We will write some intermediate files to meta.dir and save.dir.
     #   those files will be deleted with snpnet::cleanUpIntermediateFiles() function.
@@ -491,18 +630,18 @@ setupConfigs <- function(configs, genotype.pfile, phenotype.file, phenotype, cov
     if(is.null(out[['gcount.full.prefix']])) out[['gcount.full.prefix']] <- file.path(
         out[['results.dir']], out[["meta.dir"]], out['gcount.basename.prefix']
     )
-    
+
     out
 }
 
 updateConfigsWithFamily <- function(configs, family){
     out <- configs
     out[['family']] <- family
-    out[['use.glmnetPlus']] <- checkGlmnetPlus(out[['use.glmnetPlus']], family)        
+    out[['use.glmnetPlus']] <- checkGlmnetPlus(out[['use.glmnetPlus']], family)
     if (is.null(out[['metric']])) out[['metric']] <- setDefaultMetric(family)
     out
 }
-                                 
+
 ## logger functions
 
 snpnetLogger <- function(message, log.time = NULL, indent=0, funcname='snpnet'){
@@ -511,7 +650,7 @@ snpnetLogger <- function(message, log.time = NULL, indent=0, funcname='snpnet'){
 }
 
 timeDiff <- function(start.time, end.time = NULL) {
-    if (is.null(end.time)) end.time <- Sys.time()    
+    if (is.null(end.time)) end.time <- Sys.time()
     paste(round(end.time-start.time, 4), units(end.time-start.time))
 }
 
